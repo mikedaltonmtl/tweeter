049d91c76d39ee360d7a8bc16ebb234fa3c5cb05
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-11-14 23:49:06 +0000

    fetch tweets with ajax
    convert timestamp using timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,59 +3,62 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+  const createTweetElement = function(tweetData) {
 
-const createTweetElement = function(tweetData) {
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <img src=${tweetData.user.avatars} alt="avatar for user ${tweetData.user.name}"/>
-        <p>${tweetData.user.name}</p>
-        <p class="handle">${tweetData.user.handle}</p>
-      </header>
-      <p class="tweet-text">${tweetData.content.text}</p>
-      <footer>
-        <p>${tweetData.created_at}</p>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </footer>
-    </article>`);
-  return $tweet;
-};
+    // use timeago to convert timestamp into 'time since...)
+    const timeSinceTweet = timeago.format(tweetData.created_at);
 
-const renderTweets = function(tweets) {
-  for (const tweet of data) {
-    // calls createTweetElement for each tweet
-    const $tweet = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').append($tweet);
-  }
-};
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <img src=${tweetData.user.avatars} alt="avatar for user ${tweetData.user.name}"/>
+          <p>${tweetData.user.name}</p>
+          <p class="handle">${tweetData.user.handle}</p>
+        </header>
+        <p class="tweet-text">${tweetData.content.text}</p>
+        <footer>
+          <p>${timeSinceTweet}</p>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </footer>
+      </article>`
+    );
+    return $tweet;
+  };
 
-renderTweets(data);
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+  // event handler for a new tweet submission
+  $("#new-tweet-form").submit(function(event) {
+
+    const str = $("form").serialize();
+    // parameters: url, data to submit, success callback
+    $.post('/tweets/', str, function() {
+      console.log('success (post): data sent:', str);
+    });
+
+    event.preventDefault();
+  });
+
+  // fetch the tweets from the http://localhost:8080/tweets page with a AJAX, GET request
+  const loadTweets = function() {
+
+    $.getJSON('/tweets', function(data) {
+      console.log('success (get), data:', data);
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
+
+}); // end of $(document).ready
