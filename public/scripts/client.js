b18880d6e7d721961a37c82461800b510dc71a43
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-11-15 20:50:10 +0000

    refactor variables from jQuery to start with $

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,11 +6,10 @@
 $(document).ready(function() {
 
   const createTweetElement = function(tweetData) {
-
     // use timeago to convert timestamp into 'time since...)
     const timeSinceTweet = timeago.format(tweetData.created_at);
 
-    const $tweet = $(`
+    const tweet = $(`
       <article class="tweet">
         <header>
           <img src=${tweetData.user.avatars} alt="avatar for user ${tweetData.user.name}"/>
@@ -26,48 +25,17 @@ $(document).ready(function() {
         </footer>
       </article>`
     );
-    return $tweet;
+    return tweet;
   };
 
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       // calls createTweetElement for each tweet
-      const $tweet = createTweetElement(tweet);
+      const tweetHtml = createTweetElement(tweet);
       // takes return value and appends it to the tweets container
-      $('#tweets-container').append($tweet);
+      $( "#tweets-container" ).append(tweetHtml);
     }
   };
-
-  // event handler for a new tweet submission
-  $( "#new-tweet-form" ).submit(function(event) {
-
-    let validTweet = true;
-
-    const tweetText = $( this ).children( "textarea#tweet-text" ).val();
-    console.log('tweetText', tweetText);
-
-    // validate for character length
-    if (tweetText.length > 140) {
-      alert('Sorry, your tweet exceeds the maximum length (140 chars)');
-      validTweet = false;
-    }
-
-    // validate for empty or null tweet
-    if (tweetText === "" || tweetText === null) {
-      alert('Please compose your tweet to submit');
-      validTweet = false;
-    }
-
-    if (validTweet) {
-      const str = $( "form" ).serialize();
-      // $.post parameters: url, data to submit, success callback
-      $.post('/tweets/', str, function() {
-        console.log('success (post): data sent:', str);
-      });
-    }
-
-    event.preventDefault();
-  });
 
   // fetch the tweets from the http://localhost:8080/tweets page with a AJAX, GET request
   const loadTweets = function() {
@@ -78,5 +46,46 @@ $(document).ready(function() {
   };
 
   loadTweets();
+
+  // this function will probably be defunct once we start using SQL tweets...
+  const loadNewTweet = function() {
+    $.getJSON('/tweets', function(data) {
+      console.log('loadNewTweet - success (get), data:', data);
+      const newTweet = [data[data.length - 1]];
+      renderTweets(newTweet);
+    });
+  };
+
+  // event handler for a new tweet submission
+  $( "#new-tweet-form" ).submit(function(event) { 
+    event.preventDefault();
+    const $tweetText = $( this ).children( "textarea#tweet-text" ).val();
+
+    // validate for character length
+    if ($tweetText.length > 140) {
+      alert('Sorry, your tweet exceeds the maximum length (140 chars)');
+      return;
+    }
+    // validate for empty or null tweet
+    if ($tweetText === "" || $tweetText === null) {
+      alert('Please compose your tweet to submit');
+      return;
+    }
+
+    const $formString = $( "form" ).serialize();
+    // $.post parameters: url, data to submit, success callback
+    $.post('/tweets/', $formString, function() {
+      console.log('success (post): data sent:', $formString);
+    })
+    .then(() => {
+      $( this ).trigger('reset');
+      $( this ).children( "textarea#tweet-text" ).trigger('input');
+      loadNewTweet();
+      // this below might be a better solution when we switch to SQL backend?
+      // $( "#tweets-container" ).empty();
+      // loadTweets();
+    });
+  });
+
 
 }); // end of $(document).ready
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
 
   $( "textarea#tweet-text" ).on("input", function() {
-
-    // const maxlength = $(this).attr("maxlength");
     const maxlength = 140;
     const currentLength = $( this ).val().length;
 
     // select counter node relative to the textarea (this)
-    const outputCounter = $( this ).parent().find( "output.counter" );
-    outputCounter.text(maxlength - currentLength);
+    const $outputCounter = $( this ).parent().find( "output.counter" );
+    $outputCounter.text(maxlength - currentLength);
 
     // add class (for CSS) if remaining characters turns negative
-    outputCounter.toggleClass("negative", currentLength > maxlength);
-
+    $outputCounter.toggleClass("negative", currentLength > maxlength);
   });
+
 });
