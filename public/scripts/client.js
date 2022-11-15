33e9a81abd4e6f6f8bd76064b5f01db229cbc5c4
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-11-15 14:49:12 +0000

    validate tweet is not empty or too long
    add word-wrap: break-word to tweet css to improve display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -39,20 +39,38 @@ $(document).ready(function() {
   };
 
   // event handler for a new tweet submission
-  $("#new-tweet-form").submit(function(event) {
+  $( "#new-tweet-form" ).submit(function(event) {
 
-    const str = $("form").serialize();
-    // parameters: url, data to submit, success callback
-    $.post('/tweets/', str, function() {
-      console.log('success (post): data sent:', str);
-    });
+    let validTweet = true;
+
+    const tweetText = $( this ).children( "textarea#tweet-text" ).val();
+    console.log('tweetText', tweetText);
+
+    // validate for character length
+    if (tweetText.length > 140) {
+      alert('Sorry, your tweet exceeds the maximum length (140 chars)');
+      validTweet = false;
+    }
+
+    // validate for empty or null tweet
+    if (tweetText === "" || tweetText === null) {
+      alert('Please compose your tweet to submit');
+      validTweet = false;
+    }
+
+    if (validTweet) {
+      const str = $( "form" ).serialize();
+      // $.post parameters: url, data to submit, success callback
+      $.post('/tweets/', str, function() {
+        console.log('success (post): data sent:', str);
+      });
+    }
 
     event.preventDefault();
   });
 
   // fetch the tweets from the http://localhost:8080/tweets page with a AJAX, GET request
   const loadTweets = function() {
-
     $.getJSON('/tweets', function(data) {
       console.log('success (get), data:', data);
       renderTweets(data);
